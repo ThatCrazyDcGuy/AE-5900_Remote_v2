221e8523cc3a4c38c52eb794fa2cd86b7163576e
Mario <113272569+ThatCrazyDcGuy@users.noreply.github.com>
2026-05-12 13:00:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -377,12 +377,14 @@ echo '{
 ' > ~/.config/Mumble/Mumble/mumble_settings.json
 
 # Autostart
+sudo touch /usr/local/bin/ae5900starter
 sudo echo '#!/bin/bash
 pavucontrol &
 sleep 10 && mumble "mumble://ae5900ADM@127.0.0.1:64738" &
 sleep 15 && lxterminal -e  python3 ~/AE5900_Remote_V2/ae_5900_v2.py
 ' > /usr/local/bin/ae5900starter
 sudo chmod +x /usr/local/bin/ae5900starter
+touch ~/..config/autostart/ae5900LXterm.desktop
 echo '[Desktop Entry]
 Name=ae5900LXTerm
 Exec=ae5900starter
