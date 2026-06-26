84dbe0880a2fa6704135c2af933767e2ebfca7ca
Mario <113272569+ThatCrazyDcGuy@users.noreply.github.com>
2026-06-26 16:25:25 +0200

    Add files via upload

---
+++ webaudioinstaller.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+
+cd ~/
+sudo apt install python3-build
+sudo apt install python3-pyaudio portaudio19-dev -y
+sudo apt install libopus0
+cd ~/AE5900_Remote_V2/
+sudo tailscale cert $(tailscale status --self --json | jq -r '.Self.DNSName' | sed 's/\.$//')
+sleep 2
+sudo chown $USER:$USER *.crt
+sudo chown $USER:$USER *.key
+chmod 644 *.crt
+chmod 600 *.key
